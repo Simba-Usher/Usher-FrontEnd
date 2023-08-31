deefc3b871f7cb444ac0d8bcecfe63edca2eaa28
mk1329 <mk1329@dgu.ac.kr>
2023-08-31 20:35:23 +0900

    Feat: 공연정보 상세보기 페이지

--- src/components/show/showDetail/InfoImgSection.jsx
+++ src/components/show/showDetail/InfoImgSection.jsx
@@ -2,15 +2,18 @@
 
 import React, { forwardRef } from 'react'
 import * as S from "../style";
+import { useNavigate } from 'react-router-dom';
 
-export const InfoImgSection = forwardRef((props, ref) => {
+export const InfoImgSection = forwardRef((shows, ref) => {
+  const navigate = useNavigate();
+
   return (
     <div ref={ref}>
       <S.InfoImgSecWrap>
         <S.TapTitle>공연 정보</S.TapTitle>
         <S.InfoImgBox>
-          <img src="/showinfodetail.png" />
-          <S.ViewDetail>
+          <S.InfoImg src="/infodetail2.jpg" alt="viewdetail"/>
+          <S.ViewDetail onClick={() => navigate(`/${shows.id}/img`)}>
             <img src='/viewdetail.png' />
             <p style={{margin: "0 10px"}}>공연 정보 상세 보기</p>
           </S.ViewDetail>
