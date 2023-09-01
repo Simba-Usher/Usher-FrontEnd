cd72124b7fcdd071addf03585dd449ec27e36d3b
mk1329 <mk1329@dgu.ac.kr>
2023-09-01 16:07:58 +0900

    Feat: ComNav 연결

--- src/components/community/communityMain/ComNav.jsx
+++ src/components/community/communityMain/ComNav.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import * as S from "./style";
+import { useNavigate } from 'react-router-dom';
 
 export const ComNav = () => {
+  const navigate = useNavigate();
+
   return (
     <>
         <S.ComNavWrap>
-            <S.ComNavSec>실시간 인기글</S.ComNavSec>
+            <S.ComNavSec onClick={() => navigate("/community")}>실시간 인기글</S.ComNavSec>
             <div>|</div>
-            <S.ComNavSec>자유</S.ComNavSec>
+            <S.ComNavSec onClick={() => navigate("/community/free")}>자유</S.ComNavSec>
             <div>|</div>
-            <S.ComNavSec>에디터 픽</S.ComNavSec>
+            <S.ComNavSec onClick={() => navigate("/community/editor")}>에디터 픽</S.ComNavSec>
             <div>|</div>
-            <S.ComNavSec>질문</S.ComNavSec>
+            <S.ComNavSec onClick={() => navigate("/community/qna")}>질문</S.ComNavSec>
             <img src="/threedot.png" alt="dot" />
         </S.ComNavWrap>
     </>
--- src/pages/community/communityMain/style.jsx
+++ src/pages/community/communityMain/style.jsx
@@ -1,7 +1,7 @@
 import { styled } from "styled-components";
 
 // CoHot.jsx
-export const CoHotWrap = styled.div`
+export const CoWrap = styled.div`
     width: 430px;
     height: 1078px;
     display: flex;
