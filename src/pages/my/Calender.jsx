8144cb732e81a104d9e2d8faec892f32d2ce29f4
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-09-02 03:53:46 +0900

    Feat: 오타... 이슈...

--- src/pages/my/Calender.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Wrapper from "../../components/Wrapper";
-
-export const Calender = () => {
-  return <Wrapper>Calender</Wrapper>;
-};
--- src/pages/my/calender/Calender.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Wrapper from "../../components/Wrapper";
-import * as S from "./style";
-
-export const Calender = () => {
-  return <Wrapper>Calender</Wrapper>;
-};
