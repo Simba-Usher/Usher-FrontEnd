28f3124d4d961fe36bffb5ca5361ff016c446fe0
mk1329 <mk1329@dgu.ac.kr>
2023-09-06 23:47:21 +0900

    Feat: 예매처 바로가기 페이지

--- src/components/show/showDetail/SiteNotice.jsx
+++ src/components/show/showDetail/SiteNotice.jsx
@@ -8,6 +8,11 @@ export const SiteNotice = () => {
             <S.Notice>
                 <div>
                     <img src="/notice.png" alt="!" />
+                    <p>예매처 안내</p>
+                </div>
+                <div>
+                예매처마다 할인 혜택이 상이할 수 있습니다
+                각 예매처에서 정확한 가격과 할인 정보를 확인하세요!
                 </div>
             </S.Notice>
         </S.SiteNoticeWrap>
