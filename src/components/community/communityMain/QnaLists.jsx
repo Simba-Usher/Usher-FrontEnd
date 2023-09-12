a97b66d02a1a350a19ae6a1f27a69104c5e27d38
mk1329 <mk1329@dgu.ac.kr>
2023-09-12 14:34:52 +0900

    Feat: 커뮤니티 디테일 페이지 중간커밋

--- src/components/community/communityMain/QnaLists.jsx
+++ src/components/community/communityMain/QnaLists.jsx
@@ -1,3 +1,5 @@
+// QnaLists.jsx
+
 import React from 'react'
 import * as S from "./style";
 import { useNavigate } from 'react-router-dom';
@@ -5,9 +7,26 @@ import { useNavigate } from 'react-router-dom';
 export const QnaLists = ({ compost }) => {
     const navigate = useNavigate();
 
+    // Detail 페이지 불러오기
+    // const [detailData, setDetailData] = useState();
+    // console.log(detailData);
+
+    // const fetchCoDetailData = async () => {
+    //     try {
+    //         const respose = await axiosInstance.get(`/api/composts/${id}`)
+    //         setDetailData(respose.data.results);
+    //         console.log(detailData);
+    //     } catch (error) {
+    //         console.log("각 id에 해당하는 디테일 페이지 가져오는 중 에러 발생", error);
+    //     }
+    // }
+    // useEffect(() => {
+    //     fetchCoDetailData();
+    // })
+
     return (
         <>
-            <S.ComListsWrap onClick={() => navigate("/community/:detailId")}>
+            <S.ComListsWrap onClick={() => navigate(`/community/${compost.id}`)}>
                 <S.QnaSec>
                     <S.Sec1Title>Q. {compost.title}</S.Sec1Title>
                     <S.Sec2Content>
--- src/pages/community/communityMain/CoQna.jsx
+++ src/pages/community/communityMain/CoQna.jsx
@@ -58,6 +58,7 @@ export const CoQna = () => {
   useEffect(() => {
     fetchAllData();
   },[]);
+
   return (
     <Wrapper>
       <Nav />
