a0461a96dc0c2c09a823c5ce30191367604aad90
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-09-15 15:29:06 +0900

    Feat: 반응별 url 수정

--- src/components/home/ReactionSection.jsx
+++ src/components/home/ReactionSection.jsx
@@ -10,51 +10,51 @@ export const ReactionSection = () => {
       <S.HomeArticle>
         <p>관람객 반응별</p>
         <S.ReacionArticle>
-          <S.PurpleBtn onClick={() => navigate("showlist")}>
+          <S.PurpleBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>스토리가
             흥미로워요
           </S.PurpleBtn>
-          <S.BlueReBtn onClick={() => navigate("showlist")}>
+          <S.BlueReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>시야
             단차가 좋아요
           </S.BlueReBtn>
-          <S.GreenReBtn onClick={() => navigate("showlist")}>
+          <S.GreenReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>장애인
             전용 시설
           </S.GreenReBtn>
-          <S.PurpleBtn onClick={() => navigate("showlist")}>
+          <S.PurpleBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>연출이
             훌륭해요
           </S.PurpleBtn>
-          <S.BlueReBtn onClick={() => navigate("showlist")}>
+          <S.BlueReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>극장 안이
             깨끗해요
           </S.BlueReBtn>
-          <S.GreenReBtn onClick={() => navigate("showlist")}>
+          <S.GreenReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>
             유아용·수유 시설
           </S.GreenReBtn>
-          <S.PurpleBtn onClick={() => navigate("showlist")}>
+          <S.PurpleBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>무대·소품
             화려해요
           </S.PurpleBtn>
-          <S.BlueReBtn onClick={() => navigate("showlist")}>
+          <S.BlueReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>음향이 잘
             들려요
           </S.BlueReBtn>
-          <S.GreenReBtn onClick={() => navigate("showlist")}>
+          <S.GreenReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>주차 공간
             여유
           </S.GreenReBtn>
-          <S.PurpleBtn onClick={() => navigate("showlist")}>
+          <S.PurpleBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>
             노래·넘버가 좋아요
           </S.PurpleBtn>
-          <S.BlueReBtn onClick={() => navigate("showlist")}>
+          <S.BlueReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>편의
             시설이 많아요
           </S.BlueReBtn>
-          <S.GreenReBtn onClick={() => navigate("showlist")}>
+          <S.GreenReBtn onClick={() => navigate("/mainposts/:genre/:location/:price_range/:date_range")}>
             <span className="material-symbols-outlined">thumb_up</span>대중교통
             이용 편리
           </S.GreenReBtn>
